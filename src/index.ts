6f44366ac5a10fe3e502739933d2c4608899893a
Thomas Ramé <thomas.rame@outlook.com>
2022-06-01 15:10:45 +0200

    chore(deps): allow managing new URLPattern standard
    Big changes due to https://github.com/adobe/react-spectrum/issues/2857

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+// // Cannot use this for now due to constraint for old `node` versions (`globalThis` not available), so we fallback to a permanent polyfill
+// if (globalThis && !globalThis.URLPattern) {
+//   await import('urlpattern-polyfill');
+// }
+
+import 'urlpattern-polyfill';
+
 export * from './middleware';
 export * from './watcher';
