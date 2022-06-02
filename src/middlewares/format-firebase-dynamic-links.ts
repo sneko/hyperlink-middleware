4f589610d14118fc4ad629e906a5c20ba8382d03
Thomas Ramé <thomas.rame@outlook.com>
2022-06-02 16:46:52 +0200

    feat(mdw): implement FormatFirebaseDynamicLinksMiddleware

---
+++ src/middlewares/format-firebase-dynamic-links.ts
@@ -0,0 +1,155 @@
+import { Middleware } from '../middleware';
+
+import platform from 'platform';
+
+/** UTM parameters allowed by Firebase */
+enum FirebaseUtmParamEnum {
+  Source = 'utm_source',
+  Medium = 'utm_medium',
+  Campaign = 'utm_campaign',
+  Content = 'utm_content',
+  Term = 'utm_term',
+}
+
+/** Ref: https://firebase.google.com/docs/dynamic-links/create-manually */
+export type DynamicLinkParam =
+  | 'apn'
+  | 'afl'
+  | 'amv'
+  | 'ibi'
+  | 'ifl'
+  | 'ius'
+  | 'ipfl'
+  | 'ipbi'
+  | 'isi'
+  | 'imv'
+  | 'efr'
+  | 'ofl'
+  | 'st'
+  | 'sd'
+  | 'si'
+  | 'utm_source'
+  | 'utm_medium'
+  | 'utm_campaign'
+  | 'utm_term'
+  | 'utm_content'
+  | 'at'
+  | 'ct'
+  | 'mt'
+  | 'pt'
+  | 'd';
+
+export interface FormatFirebaseDynamicLinksOptions {
+  dynamicLinkBase: string; // E.g. `https://example.page.link`, this comes from Firebase (either their generated URL or a custom domain)
+  android?: {
+    storeId: string; // E.g. `com.example.app`
+    storeLink?: string; // E.g. `https://play.google.com/store/apps/details?id=com.example.app`
+  };
+  ios?: {
+    bundleId: string; // E.g. `com.example.app`
+    nativeScheme?: string; // E.g. `example` in case you use deep linking like `example://product?id=123`
+    storeId: string; // E.g. `com.example.app`
+    storeLink?: string; // E.g. `https://apps.apple.com/us/app/my-app/id0000000000`
+    skipAppPreviewPage?: string; // Corresponds to the `efr` parameter. Default `1`
+  };
+  injectUtmParamsInDynamicLink?: boolean; // The UTM parameters from the hyperlink will be injected into the Dynamic Link. Default `true`
+  injectUtmParamsInFallback?: boolean; // The UTM paramaters from the hyperlink will be injected into the fallback. Default `true`
+  usePlatformLinkAsFallback?: boolean; // By default the fallback is the hyperlink but we can force using the `ofl` parameter to redirect to the store website. Default `false`
+  overrideParams?: Partial<Record<DynamicLinkParam, string>>; // In case you want to use extra parameters of Firebase not yet used in this library you are free to do so in this object
+}
+
+/** UTM middleware to inject predefined parameters into links depending on options */
+export function FormatFirebaseDynamicLinksMiddleware(
+  options: FormatFirebaseDynamicLinksOptions
+): Middleware {
+  return (properties, element, next) => {
+    try {
+      const url = new URL(properties.href);
+      let androidStoreUrl: URL | undefined;
+      let iosStoreUrl: URL | undefined;
+
+      const dynamicLink = new URL(options.dynamicLinkBase);
+      dynamicLink.searchParams.set('link', properties.href);
+
+      if (options.android) {
+        dynamicLink.searchParams.set('apn', options.android.storeId);
+
+        if (options.android.storeLink) {
+          androidStoreUrl = new URL(options.android.storeLink);
+        }
+      }
+
+      if (options.ios) {
+        dynamicLink.searchParams.set('isi', options.ios.storeId);
+        dynamicLink.searchParams.set('ibi', options.ios.bundleId);
+
+        if (options.ios.storeLink) {
+          iosStoreUrl = new URL(options.ios.storeLink);
+        }
+
+        if (options.ios.nativeScheme) {
+          dynamicLink.searchParams.set('ius', options.ios.nativeScheme);
+        }
+
+        dynamicLink.searchParams.set(
+          'efr',
+          options.ios.skipAppPreviewPage || '1'
+        );
+      }
+
+      if (
+        options.injectUtmParamsInDynamicLink ||
+        options.injectUtmParamsInFallback
+      ) {
+        const urlParams = url.searchParams;
+
+        // Get UTM parameters from the original link
+        Object.values(FirebaseUtmParamEnum).forEach(utmKey => {
+          const utmValue = urlParams.get(utmKey);
+
+          if (utmValue) {
+            if (options.injectUtmParamsInDynamicLink) {
+              dynamicLink.searchParams.set(utmKey, utmValue);
+            }
+
+            if (options.injectUtmParamsInFallback) {
+              if (androidStoreUrl) {
+                androidStoreUrl.searchParams.set(utmKey, utmValue);
+              }
+
+              if (iosStoreUrl) {
+                iosStoreUrl.searchParams.set(utmKey, utmValue);
+              }
+            }
+          }
+        });
+      }
+
+      // Fallback link (after UTM manipulations because they can be affected)
+      if (options.usePlatformLinkAsFallback !== false) {
+        if ((platform as any).os.family === 'iOS' && iosStoreUrl) {
+          dynamicLink.searchParams.set('ofl', iosStoreUrl.toString());
+        } else if (
+          (platform as any).os.family === 'Android' &&
+          androidStoreUrl
+        ) {
+          dynamicLink.searchParams.set('ofl', androidStoreUrl.toString());
+        }
+      }
+
+      if (options.overrideParams) {
+        for (const [paramKey, paramValue] of Object.entries(
+          options.overrideParams
+        )) {
+          dynamicLink.searchParams.set(paramKey, paramValue);
+        }
+      }
+
+      properties.href = dynamicLink.toString();
+    } catch (err) {
+      // In case it's not a valid format the middleware is skipped without affecting next ones
+    }
+
+    next();
+  };
+}
--- test/middlewares/global.spec.ts
+++ test/middlewares/global.spec.ts
@@ -3,9 +3,16 @@ import {
   Middleware,
   MiddlewareComposition,
 } from '../../src';
+import { FormatFirebaseDynamicLinksMiddleware } from '../../src/middlewares/format-firebase-dynamic-links';
 import { IgnoreFollowingsMiddleware } from '../../src/middlewares/ignore-followings';
 import { SetMissingUrlProtocolMiddleware } from '../../src/middlewares/set-missing-url-protocol';
 import { SetUtmParametersMiddleware } from '../../src/middlewares/set-utm-parameters';
+
+// Info: there is no way to mock its exported object (since not a function...)
+// so we override during tests directly the object while resetting just in case in `afterEach()`
+import platform from 'platform';
+
+const platformOsBackup = JSON.parse(JSON.stringify(platform.os));
 
 describe('Middlewares', () => {
   let mdwComposition: MiddlewareComposition;
@@ -17,6 +24,9 @@ describe('Middlewares', () => {
       href: 'https://example.com',
       target: '_blank',
     };
+
+    // Used so modification on it are reset after each test
+    platform.os = JSON.parse(JSON.stringify(platformOsBackup));
   });
 
   describe('SetMissingUrlProtocolMiddleware', () => {
@@ -165,6 +175,150 @@ describe('Middlewares', () => {
       const url = new URL(properties.href);
       expect(url.searchParams.get('utm_source')).toBe('hello');
       expect(url.searchParams.get('utm_campaign')).toBeNull();
+    });
+  });
+
+  describe('FormatFirebaseDynamicLinksMiddleware', () => {
+    it('should embed the link with nothing specific (not a valid use case)', () => {
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+        })
+      );
+
+      const properties = mdwComposition.apply(initialProperties);
+
+      expect(properties.href).toBe(
+        'http://example.page.link/?link=https%3A%2F%2Fexample.com'
+      );
+    });
+
+    it('should generate the link with required parameters', () => {
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+          android: {
+            storeId: 'test_android_1',
+          },
+          ios: {
+            bundleId: 'test_ios_1',
+            storeId: 'test_ios_2',
+          },
+        })
+      );
+
+      const properties = mdwComposition.apply(initialProperties);
+      const dynamicLink = new URL(properties.href);
+
+      expect(dynamicLink.searchParams.get('link')).toBe('https://example.com');
+      expect(dynamicLink.searchParams.get('apn')).toBe('test_android_1');
+      expect(dynamicLink.searchParams.get('ibi')).toBe('test_ios_1');
+      expect(dynamicLink.searchParams.get('isi')).toBe('test_ios_2');
+    });
+
+    it('should generate the link with fallback depending on the platform', () => {
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+          android: {
+            storeId: 'test_android_1',
+            storeLink: 'http://android.com/abc',
+          },
+          ios: {
+            bundleId: 'test_ios_1',
+            storeId: 'test_ios_2',
+            storeLink: 'http://ios.com/abc',
+          },
+          usePlatformLinkAsFallback: true,
+        })
+      );
+
+      // Force Android platform
+      (platform as any).os.family = 'Android';
+
+      const properties = mdwComposition.apply(initialProperties);
+      const dynamicLink = new URL(properties.href);
+
+      // Force iOS platform
+      (platform as any).os.family = 'iOS';
+
+      const properties2 = mdwComposition.apply(initialProperties);
+      const dynamicLink2 = new URL(properties2.href);
+
+      expect(dynamicLink2.searchParams.get('ofl')).toBe('http://ios.com/abc');
+    });
+
+    it('should generate the link with UTM parameters accordingly', () => {
+      initialProperties.href =
+        'http://example.com/?utm_source=test1&utm_medium=test2&utm_campaign=test3';
+
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+          android: {
+            storeId: 'test_android_1',
+            storeLink: 'http://android.com/abc',
+          },
+          injectUtmParamsInDynamicLink: true,
+          injectUtmParamsInFallback: true,
+          usePlatformLinkAsFallback: true,
+        })
+      );
+
+      // Force Android platform
+      (platform as any).os.family = 'Android';
+
+      const properties = mdwComposition.apply(initialProperties);
+      const dynamicLink = new URL(properties.href);
+
+      expect(dynamicLink.searchParams.get('utm_source')).toBe('test1');
+      expect(dynamicLink.searchParams.get('utm_medium')).toBe('test2');
+      expect(dynamicLink.searchParams.get('utm_campaign')).toBe('test3');
+
+      const fallbackUrl = dynamicLink.searchParams.get('ofl');
+      expect(fallbackUrl).not.toBeNull();
+      const fallbackLink = new URL(fallbackUrl!);
+
+      expect(fallbackLink.searchParams.get('utm_source')).toBe('test1');
+      expect(fallbackLink.searchParams.get('utm_medium')).toBe('test2');
+      expect(fallbackLink.searchParams.get('utm_campaign')).toBe('test3');
+    });
+
+    it('should override specific parameters after generating the link', () => {
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+          android: {
+            storeId: 'test_android_1',
+          },
+          overrideParams: {
+            apn: 'changed',
+          },
+        })
+      );
+
+      const properties = mdwComposition.apply(initialProperties);
+      const dynamicLink = new URL(properties.href);
+
+      expect(dynamicLink.searchParams.get('apn')).toBe('changed');
+    });
+
+    it('should skip app preview for iOS', () => {
+      mdwComposition.add(
+        FormatFirebaseDynamicLinksMiddleware({
+          dynamicLinkBase: 'http://example.page.link',
+          ios: {
+            bundleId: 'test1',
+            storeId: 'test2',
+            skipAppPreviewPage: '1',
+          },
+        })
+      );
+
+      const properties = mdwComposition.apply(initialProperties);
+      const dynamicLink = new URL(properties.href);
+
+      expect(dynamicLink.searchParams.get('efr')).toBe('1');
     });
   });
 });
